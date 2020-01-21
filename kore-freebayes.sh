debdf1af5bf317f9e81cf10bee88d8f487c07232
adrianodemarino <adriano.demarino@gmail.com>
2020-01-21 18:19:52 +0100

    sono stanco voglio andare a casa

--- kore-freebayes.sh
+++ kore-freebayes.sh
@@ -22,7 +22,7 @@ echo "freebayes"
 
 FILE=$outdir/${idsample}.chr$c.vcf.gz
 if test -f "$FILE"; then
-    echo "$FILE exist"
+    echo ">>> $FILE exist"
 else 
     singularity exec -B $mount_dir_singu $biotools freebayes -f $genome2 -r chr$c -g 2000  $outdir/$idsample/{idsample}.bam | bgzip > $outdir/$idsample/{idsample}.chr$c.vcf.gz
     tabix -p vcf $outdir/$idsample/{idsample}.chr$c.vcf.gz
@@ -31,7 +31,7 @@ fi
 echo "quality filter"
 FILE=$outdir/${idsample}.chr$c.fb.filt.vcf
 if test -f "$FILE"; then
-    echo "$FILE exist"
+    echo ">>> $FILE exist"
 else 
     zcat $outdir/$idsample/{idsample}.chr$c.vcf.gz | singularity exec -B $mount_dir_singu $biotools vcffilter -f "QUAL > 20" > $outdir/$idsample/{idsample}.chr$c.fb.filt.vcf
 	bgzip $outdir/$idsample/{idsample}.chr$c.fb.filt.vcf
@@ -41,7 +41,7 @@ fi
 echo "normalize"
 FILE=$outdir/${idsample}.chr$c.fb.norm.vcf.gz
 if test -f "$FILE"; then
-    echo "$FILE exist"
+    echo ">>> $FILE exist"
 else 
 	singularity exec -B $mount_dir_singu $biotools vt normalize -n $outdir/$idsample/{idsample}.chr$c.fb.filt.vcf.gz -r $genome2 -o $outdir/$idsample/{idsample}.chr$c.fb.norm.vcf.gz 
 	tabix -p vcf $outdir/$idsample/{idsample}.chr$c.fb.norm.vcf.gz
@@ -50,7 +50,7 @@ fi
 echo "decompose"
 FILE=$outdir/${idsample}.chr$c.fb.norm.decompose.vcf.gz
 if test -f "$FILE"; then
-    echo "$FILE exist"
+    echo ">>> $FILE exist"
 else 
 	singularity exec -B $mount_dir_singu $biotools vt decompose_blocksub  $outdir/$idsample/{idsample}.chr$c.fb.norm.vcf.gz  -o $outdir/$idsample/{idsample}.chr$c.fb.norm.decompose.vcf.gz
 	tabix -p vcf $outdir/$idsample/{idsample}.chr$c.fb.norm.decompose.vcf.gz
