94b83b0e8adb6dd43ad77a39c360658e292f1416
adrianodemarino <adriano.demarino@gmail.com>
2020-01-21 13:58:52 +0100

    add variables

--- variantCalling.sh
+++ variantCalling.sh
@@ -6,8 +6,9 @@ biotools=/home/adriano/src/img/biotools.img
 mount_dir_singu=/home/adriano/
 inputdir=/opt/NGS/data/memorial_hospital
 outdir=/opt/NGS/results/memorial_hospital
-
-for idsample in $(cat sample_list.txt); 
+sample_list=/opt/NGS/data/sample_list.txt
+chr_list=/opt/NGS/data/chr_list.txt
+for idsample in $(cat $sample_list); 
 	
 	do 
 	idsample_R1=`ls *$idsample*_1*`
@@ -27,16 +28,19 @@ for idsample in $(cat sample_list.txt);
 	echo " sambamba remove duplicates "
 	singularity exec -B $mount_dir_singu $biotools sambamba markdup -t 8 -p --tmpdir $tmpdir --overflow-list-size 500000 $outdir/${idsample}.raw.sorted.bam $outdir/${idsample}.bam
 	#7.
-	for c in $(cat chr_list.txt); do  
+	for c in $(cat $chr_list); do
+		echo " freebayes call variant per chromosome "
 		singularity exec -B $mount_dir_singu $biotools freebayes -f $genome -r chr$c -g 2000  $outdir/${idsample}.bam | bgzip > $outdir/${idsample}.chr$c.vcf.gz
 		tabix -p vcf $outdir/${idsample}.chr$c.vcf.gz
 		zcat $outdir/${idsample}.chr$c.vcf.gz | singularity exec -B $mount_dir_singu $biotools vcffilter -f "QUAL > 20" > $outdir/${idsample}.chr$c.fb.filt.vcf
 		bgzip $outdir/${idsample}.chr$c.fb.filt.vcf
 		tabix -p vcf $outdir/${idsample}.chr$c.fb.filt.vcf.gz
 		#10.
+		echo " normalization "
 		singularity exec -B $mount_dir_singu $biotools vt normalize -n $outdir/${idsample}.chr$c.fb.filt.vcf.gz -r $genome -o $outdir/${idsample}.chr$c.fb.norm.vcf.gz 
 		tabix -p vcf $outdir/${idsample}.chr$c.fb.norm.vcf.gz
 		#11.
+		echo " decompose "
 		singularity exec -B $mount_dir_singu $biotools vt decompose_blocksub  $outdir/${idsample}.chr$c.fb.norm.vcf.gz  -o $outdir/${idsample}.chr$c.fb.norm.decompose.vcf.gz
 		tabix -p vcf $outdir/${idsample}.chr$c.fb.norm.decompose.vcf.gz
 
