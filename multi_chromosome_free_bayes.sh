ce9d872449cc853e0e47bab1c6f792295116f344
adrianodemarino <adriano.demarino@gmail.com>
2020-01-22 16:48:04 +0100

    quote error

--- multi_chromosome_free_bayes.sh
+++ multi_chromosome_free_bayes.sh
@@ -6,7 +6,7 @@ chr_list=/opt/NGS/data/chr_list.txt
 
 
 for id in $(cat $sample_list); do
-	echo \tsample ID: $id\t
+	echo "sample ID: $id"
 	for c in $(cat $chr_list); do 
 		/home/$(whoami)/github/variantCalling/kore-freebayes.sh -i $id -c $c & 
 	done
