3f8381c416702916691e9e147b3edfe1537eb280
adrianodemarino <adriano.demarino@gmail.com>
2020-01-22 11:36:22 +0100

    add repo for bioinfo

--- kore-freebayes.sh
+++ kore-freebayes.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#https://bioinformaticsworkbook.org/dataAnalysis/VariantCalling/freebayes-dnaseq-workflow.html
+#tutotial for variant calling
 genome=/opt/NGS/genomes/hg38.fa.gz
 genome2=/opt/NGS/genomes/hg38.p12.fa
 tmpdir=/opt/NGS/scratch/
@@ -23,18 +25,18 @@ echo "freebayes"
 
 FILE=$outdir/$idsample.chr$c.vcf.gz
 if test -f "$FILE"; then
-    echo ">>> $FILE exist"
+	echo ">>> $FILE exist"
 else 
-    singularity exec -B $mount_dir_singu $biotools freebayes -f $genome2 -r chr$c -g 2000  $outdir/$idsample/$idsample.bam | bgzip > $outdir/$idsample/$idsample.chr$c.vcf.gz
-    tabix -p vcf $outdir/$idsample/$idsample.chr$c.vcf.gz
+	singularity exec -B $mount_dir_singu $biotools freebayes -f $genome2 -r chr$c -g 2000  $outdir/$idsample/$idsample.bam | bgzip > $outdir/$idsample/$idsample.chr$c.vcf.gz
+	tabix -p vcf $outdir/$idsample/$idsample.chr$c.vcf.gz
 fi
 
 echo "quality filter"
 FILE=$outdir/$idsample.chr$c.fb.filt.vcf
 if test -f "$FILE"; then
-    echo ">>> $FILE exist"
+	echo ">>> $FILE exist"
 else 
-    zcat $outdir/$idsample/$idsample.chr$c.vcf.gz | singularity exec -B $mount_dir_singu $biotools vcffilter -f "QUAL > 20" > $outdir/$idsample/$idsample.chr$c.fb.filt.vcf
+	zcat $outdir/$idsample/$idsample.chr$c.vcf.gz | singularity exec -B $mount_dir_singu $biotools vcffilter -f "QUAL > 20" > $outdir/$idsample/$idsample.chr$c.fb.filt.vcf
 	bgzip $outdir/$idsample/$idsample.chr$c.fb.filt.vcf
 	tabix -p vcf $outdir/$idsample/$idsample.chr$c.fb.filt.vcf.gz
 fi
@@ -42,7 +44,7 @@ fi
 echo "normalize"
 FILE=$outdir/$idsample.chr$c.fb.norm.vcf.gz
 if test -f "$FILE"; then
-    echo ">>> $FILE exist"
+	echo ">>> $FILE exist"
 else 
 	singularity exec -B $mount_dir_singu $biotools vt normalize -n $outdir/$idsample/$idsample.chr$c.fb.filt.vcf.gz -r $genome2 -o $outdir/$idsample/$idsample.chr$c.fb.norm.vcf.gz 
 	tabix -p vcf $outdir/$idsample/$idsample.chr$c.fb.norm.vcf.gz
@@ -51,7 +53,7 @@ fi
 echo "decompose"
 FILE=$outdir/$idsample.chr$c.fb.norm.decompose.vcf.gz
 if test -f "$FILE"; then
-    echo ">>> $FILE exist"
+	echo ">>> $FILE exist"
 else 
 	singularity exec -B $mount_dir_singu $biotools vt decompose_blocksub  $outdir/$idsample/$idsample.chr$c.fb.norm.vcf.gz  -o $outdir/$idsample/$idsample.chr$c.fb.norm.decompose.vcf.gz
 	tabix -p vcf $outdir/$idsample/$idsample.chr$c.fb.norm.decompose.vcf.gz
