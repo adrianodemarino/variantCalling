7acc74de280ac524ee6c76c07259f33abed05c50
adrianodemarino <adriano.demarino@gmail.com>
2020-01-27 13:01:04 +0100

    asd

--- multi_chromosome_free_bayes.sh
+++ multi_chromosome_free_bayes.sh
@@ -4,25 +4,8 @@ sample_list=/opt/NGS/data/priority_sample_list.txt
 chr_list=/opt/NGS/data/chr_list.txt
 
 samples=(
-181755
-182151
-190210
-190423
-190810
-191080
-191386
-191397
-192127
-192130
-192152
-192186
 192361
-192369
-192682
-192805
-192824
-192897
-193012)
+192824)
 
 
 for id in "${samples[@]}"; do
