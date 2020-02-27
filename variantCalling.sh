e8c0e221c25600c6d17146edef51539a9a3c3ff5
adrianodemarino <adriano.demarino@gmail.com>
2020-02-27 13:12:02 +0100

    var calling

--- variantCalling.sh
+++ variantCalling.sh
@@ -5,8 +5,8 @@ tmpdir=/scratch
 biotools=/home/adriano/img/biotools.img
 mount_dir_singu=/data
 inputdir=/data/research/NGS/data/clinical_exome_rappresentative/fastq
-outdir=/data/research/NGS/data/clinical_exome_rappresentative/bam_hg38
-sample_list=/data/research/NGS/data/clinical_exome_rappresentative/fastq/sample_list.list
+outdir=/data/research/NGS/data/clinical_exome_rappresentative/coverage
+sample_list=/data/research/NGS/results/memorial_exome/missing_sample.list
 
 for idsample in `cat $sample_list`; do
     dir=$outdir/$idsample
@@ -15,22 +15,22 @@ for idsample in `cat $sample_list`; do
         echo ">>> SKIP"
     else 
         mkdir $outdir/$idsample
-        idsample_R1=`ls $inputdir/*$idsample*r1*`
-        idsample_R2=`ls $inputdir/*$idsample*r2*`
+        idsample_R1=`ls $inputdir/*$idsample* | head -1`
+        idsample_R2=`ls $inputdir/*$idsample* | tail -1`
         echo Start analysis of sample $idsample_R1 $idsample_R2
         #1. Dowload human hg38 genome
         #2. index it with: `bwa index -a bwtsw hg38.fa.gz`
         #3. mapping
         echo "> mapping bwa "
-        bwa mem -t 20 -R "@RG\tID:$idsample\tSM:$idsample" $genome $idsample_R1 $idsample_R2 |  samtools view -b - > $outdir/$idsample/$idsample.raw.bam
+        bwa mem -t 30 -R "@RG\tID:$idsample\tSM:$idsample" $genome $idsample_R1 $idsample_R2 |  samtools view -b - > $outdir/$idsample/$idsample.raw.bam
         #4. View
         #singularity exec -B $mount_dir_singu $biotools samtools view -h $outdir/$idsample/$idsample.raw.bam | less -S
         #5. 
         echo "> sambamba sort "
-        sambamba sort -t 20 -m 25G --tmpdir $tmpdir -o $outdir/$idsample/${idsample}.raw.sorted.bam $outdir/$idsample/${idsample}.raw.bam
+        sambamba sort -t 30 -m 25G --tmpdir $tmpdir -o $outdir/$idsample/${idsample}.raw.sorted.bam $outdir/$idsample/${idsample}.raw.bam
         #6. mark duplicates
         echo "> sambamba remove duplicates "
-        sambamba markdup -t 20 -p --tmpdir $tmpdir --overflow-list-size 500000 $outdir/$idsample/${idsample}.raw.sorted.bam $outdir/$idsample/${idsample}.bam
+        sambamba markdup -t 30 -p --tmpdir $tmpdir --overflow-list-size 500000 $outdir/$idsample/${idsample}.raw.sorted.bam $outdir/$idsample/${idsample}.bam
         #7.
     fi
 done
